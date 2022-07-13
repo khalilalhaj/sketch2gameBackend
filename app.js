df120585545e61a3531f6f07c4c61811fb8102d0
khalilAhaj <khalilhaj@protonmail.com>
2022-07-13 17:21:33 +0300

    add more functionalities

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+} from "firebase/firestore/lite";
+
 const app = express();
 const port = 3000;
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBYVyrGFnbrB--gEX2SjlgHdYhD6tPnX7g",
+  authDomain: "sketch2game.firebaseapp.com",
+  projectId: "sketch2game",
+  storageBucket: "sketch2game.appspot.com",
+  messagingSenderId: "603083634642",
+  appId: "1:603083634642:web:ee9c657293b994ea167a0b",
+  measurementId: "G-W83HL0NSZL",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("Message sent");
+  res.json("Message sent");
 });
+
+app.get("/levels", async (req, res) => {
+  const snapshot = await getDocs(collection(db, "levels"));
+  const cityList = snapshot.docs.map((doc) => doc.data());
+  res.send(cityList);
+});
+
+app.post("/postLevel", async (req, res) => {
+  try {
+    const { id } = await addDoc(collection(db, "levels"), req.body);
+    return res.send(id);
+  } catch (error) {
+    return console.log(error);
+  }
+  return;
+});
+
+// app.get("/levels/:id", (req, res) => {});
 
 app.listen(process.env.PORT || port, () => {
   console.log(`app listening on port ${port}`);
