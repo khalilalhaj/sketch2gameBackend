9f6d1c6afe4b044ebaff453d62c41b48e56d843a
khalilAhaj <khalilhaj@protonmail.com>
2022-07-13 14:33:25 +0300

    changing text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Fuck You Ezekiel");
+  res.send("Message sent");
 });
 
 app.listen(process.env.PORT || port, () => {
